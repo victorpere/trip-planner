b05a651163409abad4e7db7c3e34d09ff8a3fdda
victorpere <paninaro095@gmail.com>
2023-01-19 16:56:04 -0500

    signin page redirect back

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
+import { Navigate } from "react-router-dom";
 
 const Signin = () => {
   const auth = useAuth();
@@ -10,7 +11,7 @@ const Signin = () => {
     }
   }, [auth]);
 
-  return null;
+  return <Navigate replace to="/" />;
 };
 
 export default Signin;
